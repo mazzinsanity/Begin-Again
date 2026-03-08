e9207be21f00108d5825bea4f30a1f6e21491913
mazzinsanity <mazzinsanity@gmail.com>
2026-03-08 19:11:23 +0100

    Other Version of Workflows

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-MAP=$1
-
-echo Testing $MAP
-
 tools/deploy.sh ci_test
 mkdir ci_test/config
 mkdir ci_test/data
 
 #test config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
-
-#set the map
-cp _maps/$MAP.json ci_test/data/next_map.json
+cp tools/ci/ci_maps.txt ci_test/config/maps.txt
 
 cd ci_test
-DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci"
+DreamDaemon hailmary.dmb -close -trusted -verbose -params "log-directory=ci"
 
 cd ..
 
-mkdir -p data/screenshots_new
-cp -r ci_test/data/screenshots_new data/screenshots_new
-
-cat ci_test/data/logs/ci/clean_run.lk
+cat ci_test/data/logs/ci/clean_run.lk # this is to print a success message or return an error on fail

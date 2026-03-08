4040d3075061eff98f56aa6c05d0f7be77c60960
mazzinsanity <mazzinsanity@gmail.com>
2026-03-08 16:57:28 +0100

    Revert "Update dependencies.sh"
    This reverts commit 1245ae332b63c40601bd2e89a69e0e88d6a7753e.

--- dependencies.sh
+++ dependencies.sh
@@ -8,16 +8,16 @@ export BYOND_MAJOR=516
 export BYOND_MINOR=1659
 
 #rust_g git tag
-export RUST_G_VERSION=6.0.0
+export RUST_G_VERSION=4.2.0
 
 #node version
-export NODE_VERSION_LTS=18.13.0
+export NODE_VERSION_LTS=14.16.1
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.11
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.11.0
+export PYTHON_VERSION=3.9.0
 
 # Auxmos repository name
 export AUXMOS_REPOSITORY=Putnam3145/auxmos
